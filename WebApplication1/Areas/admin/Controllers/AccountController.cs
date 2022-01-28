5849a38c967da5f5e409654a6f05a1092f8468fd
ClassTime <classtime@code.local>
2022-01-28 18:32:54 +0400

    second

---
+++ WebApplication1/Areas/admin/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly SignInManager<CustomUser> _signInManager;
+        private readonly UserManager<CustomUser> _userManager;
+
+        public AccountController(AppDbContext context,SignInManager<CustomUser> signInManager,UserManager<CustomUser> userManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public async Task<IActionResult> Login(VmUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model != null)
+                {
+                  
+                }
+            }
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ WebApplication1/ViewModels/MaxLenghtAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebApplication1.ViewModels
+{
+    internal class MaxLenghtAttribute : Attribute
+    {
+    }
+}
